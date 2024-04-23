20096e534e191ed0c206a6da0e5b52b92597db37
Mauros Milach Junior <mauros99milach@gmail.com>
2024-04-23 09:22:16 -0300

    feat: created possible moves of a piece logic

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,30 +1,35 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
-	private Piece piece[][]; 
 	private Board board;
 
-	public Piece (Board board) {
+	public Piece(Board board) {
 		this.board = board;
 		this.position = null;
-	}
-	
-	public Piece[][] possibleMoves (){
-		return this.piece;
-	}
-	
-	public boolean isThereAnyPossibleMove() {
-		return true;
-	}
-	
-	public boolean possibleMove (Position position) {
-		return true;
 	}
 
 	protected Board getBoard() {
 		return board;
 	}
-	
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 }
